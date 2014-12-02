af46fb9b1ec7991a08a15c42160d8359bbfb75c7
Keck Jean-Baptiste <jbkeck@hotmail.com>
2014-12-02 19:02:38 +0100

    added local bin/include/lib

--- script/scanner.sh
+++ script/scanner.sh
@@ -4,8 +4,8 @@
 
 hosts=""
 for i in {10..338}; do
-    hosts=$hosts"ensipc"$i" "
+    hosts="$hosts ensipc$i"
 done
-#echo $hosts
+echo $hosts
 #nmap -sP -iL hostfile 2>/dev/null | grep "Nmap scan" | awk -F" " '{print $5}'
 nmap -sP $hosts 2>/dev/null | grep "Nmap scan" | awk -F" " '{print $5}' >> onlineHosts
