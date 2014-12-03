5b90b0f8d26d32f18bb50be6c93714961b8f9305
Gauthier Zirnhelt <gauthier.zirnhelt@ensimag.imag.fr>
2014-12-03 10:37:03 +0100

    ensipsys dans scanner

--- script/scanner.sh
+++ script/scanner.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-> onlineHosts
-
 hosts=""
 for i in {10..338}; do
     hosts="$hosts ensipc$i"
 done
-echo $hosts
+for i in {1..100}; do
+    hosts="$hosts ensipsys$i"
+done
+#echo $hosts
 #nmap -sP -iL hostfile 2>/dev/null | grep "Nmap scan" | awk -F" " '{print $5}'
-nmap -sP $hosts 2>/dev/null | grep "Nmap scan" | awk -F" " '{print $5}' >> onlineHosts
+nmap -sP $hosts 2>/dev/null | grep "Nmap scan" | awk -F" " '{print $5}' > onlineHosts
