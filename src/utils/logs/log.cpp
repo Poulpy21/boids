0c63365a476f2dd18983c36ea053f91cdc1389e6
Keck Jean-Baptiste <jbkeck@hotmail.com>
2015-01-22 17:19:46 +0100

    log cmake version

--- src/utils/logs/log.cpp
+++ src/utils/logs/log.cpp
@@ -24,24 +24,20 @@ namespace log4cpp {
 
     void initLogs() {
 
-        log_console->setPriority(Priority::_CONSOLE_LOG_LEVEL);
-        log_file->setPriority(Priority::_FILE_LOG_LEVEL);
-
-        log4cpp::Appender *appender_console = new log4cpp::OstreamAppender("console", &std::cout);
+        log_console->setPriority(Priority::CONSOLE_LOG_LEVEL);
+        log_file->setPriority(Priority::FILE_LOG_LEVEL);
 
         log4cpp::PatternLayout *layout = new log4cpp::PatternLayout();
         layout->setConversionPattern("%d{%H:%M:%S} %p %c %x: %m%n");
 
+        log4cpp::Appender *appender_console = new log4cpp::OstreamAppender("console", &std::cout);
         appender_console->setLayout(layout);
         appender_console->setThreshold(Priority::DEBUG);
         log_console->addAppender(appender_console);
 
         log4cpp::Appender *appender_file = new log4cpp::FileAppender("default", "program.log");
-        appender_file->setLayout(new log4cpp::BasicLayout());
+        appender_file->setLayout(layout);
         appender_file->setThreshold(Priority::DEBUG);
-        //log_file->addAppender(appender_file);
-
-        //console redirected to file
         log_console->addAppender(appender_file);
     }
 }
